c1f7951262cdf333b177d2125fb9611130a994e9
Rotsy28 <rotsy.navalona28@gmail.com>
2026-04-09 15:09:39 +0200

    classe humain et commercant

---
+++ src/personnages/Commercant.java
@@ -0,0 +1,21 @@
+package personnages;
+
+public class Commercant extends Humain {
+	
+	public Commercant(String nom, String boisson, int argent) {
+		super(nom, "thé" , argent);
+	}
+	
+	public int seFaireExtorquer() {
+		int somme = argent;
+		argent = 0;
+		parler("J'ai tout perdu! Le monde est vraiment trop injuste...");
+		return somme;
+	}
+	
+	public void recevoir( int argent) {
+		gagnerArgent(argent);
+		parler( argent + " sous!Je te remercie généreux donateur!");
+	}
+
+}
---
+++ src/personnages/Humain.java
@@ -0,0 +1,53 @@
+package personnages;
+
+public class Humain {
+	
+	private String nom;
+	private String boisson;
+	protected int argent= 0;
+	
+	public Humain(String nom, String boisson, int argent) {
+		this.nom = nom;
+		this.boisson = boisson;
+		this.argent = argent;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getArgent() {
+		return argent;
+	}
+
+	
+	protected void gagnerArgent(int gain) {
+		argent+=gain;
+	}
+	
+	protected void perdreArgent( int perte) {
+		argent-=perte;
+	}
+	
+	public void parler( String texte) {
+		System.out.println("(" + nom + ")-" + texte);
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour!Je m'appelle " + nom + " et j'aime boire du " + boisson);
+	}
+	
+	public void boire() {
+		parler("Mmm, un bon verre de " + boisson+ "! GLOUPS!");
+	}
+	
+	public void acheter(String bien, int prix) {
+		if (argent >= prix) {
+			parler("J'ai " + argent + " sous en poche. Je vais pouvoir m'offir un " + bien + " à " + prix + " sous.");
+			argent-=prix;
+		}
+		else {
+			parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir un " + bien+ " à "+ prix + " sous.");
+		}
+	}
+}
